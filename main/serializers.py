555b80abf1303f7f3acef7516a8ca0c0ea407704
Game-devel1 <rahatsadykov784@gmail.com>
2020-05-27 15:13:58 +0600

    products post api done

--- main/serializers.py
+++ main/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Products
 
-class ProductsSerializer(serializers.Serializer):
+class ProductsSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(read_only=True)    
     name = serializers.CharField(max_length=100)
     price = serializers.IntegerField()
     details = serializers.CharField(max_length=100)
     link_img = serializers.ImageField(max_length=None, use_url=True)
+
+    class Meta:
+        model = Products
+        fields = '__all__'        
--- main/views.py
+++ main/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FileUploadParser, FormParser
 from .models import Products
 from .serializers import ProductsSerializer
 
 class ProductsView(APIView):
-    permission_classes = ()
+    parser_classes = (MultiPartParser, FormParser)
     def get(self,request, *args, **kwargs):
         products = Products.objects.all()
         result = {}
@@ -18,7 +19,7 @@ class ProductsView(APIView):
 
     def post(self,request,*args,**kwargs):
         file_serializer = ProductsSerializer(data=request.data)
-        if file_serializer.is_valid():
+        if file_serializer.is_valid(raise_exception=True):
             file_serializer.save()
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
